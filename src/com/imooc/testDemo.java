1af38502282c7e3a429580638db10741fec85231
Lu <490185999@qq.com>
2020-01-30 21:09:53 +0800

    20200130 2108

---
+++ src/com/imooc/testDemo.java
@@ -0,0 +1,11 @@
+package com.imooc;
+
+public class testDemo {
+    public String name;
+    public int age;
+
+    public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("Hi,Lu");
+    }
+}
